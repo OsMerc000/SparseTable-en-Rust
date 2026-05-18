b9c68b434c1cd9317fdb13eadaa9d0430ba4258e
Johanes Osmayo <joh.osm@gmail.com>
2026-05-18 13:16:21 -0500

    Explicación de la creación del SparseTable

--- src/sparse_table.rs
+++ src/sparse_table.rs
@@ -1,34 +1,111 @@
 pub use std::fmt::Display;
 
+//Un SparseTable lo podemos construir como un vector de vectores
 pub struct SparseTable<T> {
     st: Vec<Vec<T>>
 }
 
+/*
+Métodos para el SparseTable
+La parte de T: PartialOrd implica que el tipo genérico T es un tipo que
+podamos comparar (<, >, ==, <=, >=) y también clonar.
+*/
 impl<T: PartialOrd + Clone> SparseTable<T> {
     pub fn new(arr: &[T]) -> Self {
         let len = arr.len();
-        let size = match len.checked_ilog2() {
-            Some(num) => num,
-            None => {
-                return Self {
-                    st: Vec::new()
-                }
-            }
+
+        /*
+        El SparseTable tendrá niveles desde 0 hasta el logaritmo entero de
+        la cantidad de elementos del array. Los niveles representan el 
+        elemento mínimo del array desde la posición j hasta la posición 
+        j + (2^i) - 1 donde i es el nivel en el que nos encontramos. O sea,
+        si tuvieramos un arr de la siguiente forma:
+        
+        arr = [1, 6, 7, 8, 2]
+
+        El nivel 0 sería el mismo array.
+        
+        El nivel 1 sería un array de los elementos mínimos de cada subarray
+        de tamaño 2. Tal que así:
+        arr1 = [min(0..1), min(1..2), min(2..3), min(3..4)]
+        arr1 = [1, 6, 7, 2]
+
+        Y el nivel 2 sería el array de elementos mínimos de cada subarray de
+        tamaño 4. Tal que así:
+        arr2 = [min(0..3), min(1..4)]
+        arr2 = [1, 2]
+
+        Nótese que, debido a lo que representa cada nivel, no puede
+        exirtir un nivel 3. Porque eso significaría un subarray de 8
+        elementos, pero nuestro array solo tiene 5 elementos. Por ende,
+        podemos concluir que la cantidad de niveles máximo el logartimo en
+        base 2 redondeado abajo de la cantidad de elementos del array.
+        */
+        let size = if arr.len() > 0 {
+            arr.len().ilog2()
+        } else {
+            //Si el array está vacío, el SparTable también lo está.
+            return Self {
+                st: Vec::new()
+            };
         };
 
+        /*
+        Alojamos la cantidad suficiente de espacio para los punteros a los
+        vectores que conformarán nuestro SparseTable. La razón por la que es
+        size + 1 es porque tenemos que incluir el nivel 0 (El mismo
+        array).
+        */
         let mut st: Vec<Vec<T>> = Vec::with_capacity(size as usize + 1);
+        //Alojamos al nivel 0 el mismo array
         st.push(arr.to_vec());
+        //Nos ubicamos en el nivel correspondiente. Size es incusivo.
         for i in 1..=size {
+            /*
+            Calculamos la cantidad de elementos que habrán en el i-ésimo
+            nivel del SparseTable. Y luego de ello alojamos el espacio
+            necesario.
+            */
             let num_elem = len - (2_usize.pow(i) - 1);
             st.push(Vec::with_capacity(num_elem));
+            /*
+            Esta parte es interesante :).
+            
+            Nos ubicamos en la posición j del nivel i. Sabemos que este
+            elemento representa el mínimo elemento entre el rango j, hasta la 
+            posición j + (2^i) - 1. Por ende, en vez de tardar buscando el 
+            mínimo elemento en el array, lo que haremos es consultar el nivel 
+            anterior del SparseTable. Ya que este ya tiene definido el mínimo 
+            elemento u del rango j hasta j + (2^(i-1)) - 1 y el mínimo 
+            elemento v del rango j + (2^(i-1)) hasta 
+            j + (2^(i-1)) + (2^(i-1)) - 1. Nótese que esta última expresión se
+            puede simplificar, tal que su tope es j + (2^i) - 1. O sea, que si
+            comparamos u y v, sería los mismo buscar el elemento mínimo desde 
+            j hasta j + (2^i) - 1. En otras palabras, nuestra misión es buscar 
+            el elemento u y v.
+            
+            Sabemos que el índice de cada elemento en cada nivel representa a
+            su vez el mínimo índice del rango al que pertenece. O sea, el
+            índice del elemento que representa al elemento mínimo en el rango
+            j..j + (2^i) - 1 es igual a j. Esto se da sin importar el nivel.
+            Por tanto, el índice de u será el mismo al del índice donde nos
+            encontramos. Para hallar v, sabiendo que su rango inicia en
+            j + (2^(i-1)) aplicamos la misma propiedad. Por ello, los
+            elementos u y v son aquellos ubicados en el nivel i-1 y cuyos
+            índices son j y j + (2^(i-1)) respectivamente.
+
+            Ahora solo debemos de comparar cuál de esos 2 es menor y sabremos
+            cuál es el mínimo elemento en el rango j hasta j + (2^i) - 1.
+            */
             for j in 0..num_elem {
-                let v = st[i as usize - 1][j].clone();
-                let u = st[i as usize - 1][j + 2_usize.pow(i - 1)].clone();
-                let min = if v < u {v} else {u};
+                let u = st[i as usize - 1][j].clone();
+                let v = st[i as usize - 1][j + 2_usize.pow(i - 1)].clone();
+                let min = if u < v {u} else {v};
                 st[i as usize].push(min);
             }
         }
 
+        //Retornamos el SparseTable
         Self {st}
     }
 
